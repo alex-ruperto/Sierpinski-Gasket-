66be3ad5bdeac555e8e1eb7848c85be57c77a942
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2024-03-26 21:28:36 -0400

    Completed the Sierpinski Gasket.

--- webgl-sierpinski-gasket.js
+++ webgl-sierpinski-gasket.js
@@ -76,3 +76,43 @@ let position = gl.getAttribLocation(shaderProgram, 'aPosition');
 gl.enableVertexAttribArray(position)
 gl.vertexAttribPointer(position, 2, gl.FLOAT, false, 0, 0);
 gl.drawArrays(gl.POINTS, 0, numPoints)
+
+function initShaderProgram(gl, vsSource, fsSource) {
+    // load/compile vertex shader using the helper function loadShader
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+
+    // load/compile fragment using the function loadShader.
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+    const shaderProgram = gl.createProgram(); // create a shader program
+    gl.attachShader(shaderProgram, vertexShader); // attach the vertex shader
+    gl.attachShader(shaderProgram, fragmentShader); // attach the fragment shader
+    gl.linkProgram(shaderProgram); // link attached shaders for the final shader program
+
+    // check if the shader program was linked successfully 
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+        return null;
+    }
+
+    return shaderProgram;
+}
+
+function loadShader(gl, type, source){
+    // create shader  obj of type VERTEX_SHADER or FRAGMENT_SHADER
+    const shader = gl.createShader(type);
+    // set source code of shader
+    gl.shaderSource(shader, source);
+    // compile the shader code into a shader
+    gl.compileShader(shader);
+
+    // check if it compiled successfully
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
+        alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
+
+}
